0e586294de90090583c4d20070854552a42bc4ca
Milan <surkos.milan@gmail.com>
2024-11-11 20:38:44 +0100

    Docs: Vyvolaj podmienku

--- kalkulacka.py
+++ kalkulacka.py
@@ -1,30 +1,33 @@
-try:
-    prve_cislo = int(input("vloz prve cislo: "))
-except(ValueError):
-    print("vlozil jsi chybnu hodnotu, bude pouzite '1'")
-    prve_cislo = 1
-
-
-znamienko = input("vloz znak pozadovanej matematickej operacie (+, -, *, /): ")
-if znamienko not in ["+", "-", "*", "/"]:
-    raise ValueError("zvolil jsi chybnu matematicku operaciu a program bude ukoncen")
-
-try:
-    druhe_cislo = int(input("vloz druhe cislo: "))
-except(ValueError):
-    print("vlozil jsi chybnu hodnotu, bude pouzite '1'")
-    druhe_cislo = 1
-
-if znamienko == "+":
-    print(f"Sucet cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo + druhe_cislo}")
-elif znamienko == "-":
-    print(f"Rozdiel cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo - druhe_cislo}")
-elif znamienko == "*":
-    print(f"Krat cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo * druhe_cislo}")
-elif znamienko == "/":
+while True:
     try:
-        print(f"podiel cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo / druhe_cislo}")
-    except (ZeroDivisionError):
-        print("nula sa neda delit")
-else:
-    print(f"Zla operacia program bude ukonceny")
+        prve_cislo = int(input("vloz prve cislo: "))
+    except(ValueError):
+        print("vlozil jsi chybnu hodnotu, bude pouzite '1'")
+        prve_cislo = 1
+
+
+    znamienko = input("vloz znak pozadovanej matematickej operacie (+, -, *, /): ")
+    if znamienko not in ["+", "-", "*", "/"]:
+        raise ValueError("zvolil jsi chybnu matematicku operaciu a program bude ukoncen")
+
+    try:
+        druhe_cislo = int(input("vloz druhe cislo: "))
+    except(ValueError):
+        print("vlozil jsi chybnu hodnotu, bude pouzite '1'")
+        druhe_cislo = 1
+
+    if znamienko == "+":
+        print(f"Sucet cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo + druhe_cislo}")
+    elif znamienko == "-":
+        print(f"Rozdiel cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo - druhe_cislo}")
+    elif znamienko == "*":
+        print(f"Krat cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo * druhe_cislo}")
+    elif znamienko == "/":
+        try:
+            print(f"podiel cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo / druhe_cislo}")
+        except (ZeroDivisionError):
+            print("nula sa neda delit")
+    else:
+        print(f"Zla operacia program bude ukonceny")
+
+
