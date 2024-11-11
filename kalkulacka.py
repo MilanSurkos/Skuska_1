619a618eaa280bf8c48b4913ba7e1f4892b7b7db
Milan <surkos.milan@gmail.com>
2024-11-11 20:16:22 +0100

    Docs: Vyvolaj podmienku

--- kalkulacka.py
+++ kalkulacka.py
@@ -1,14 +1,30 @@
-prve_cislo = int(input("vloz prve cislo: "))
-znamienko = input("vloz znak pozadovane matematicke operace (+, -, *, /): ")
-druhe_cislo = int(input("vloz druhe coslo: "))
+try:
+    prve_cislo = int(input("vloz prve cislo: "))
+except(ValueError):
+    print("vlozil jsi chybnu hodnotu, bude pouzite '1'")
+    prve_cislo = 1
+
+
+znamienko = input("vloz znak pozadovanej matematickej operacie (+, -, *, /): ")
+if znamienko not in ["+", "-", "*", "/"]:
+    raise ValueError("zvolil jsi chybnu matematicku operaciu a program bude ukoncen")
+
+try:
+    druhe_cislo = int(input("vloz druhe cislo: "))
+except(ValueError):
+    print("vlozil jsi chybnu hodnotu, bude pouzite '1'")
+    druhe_cislo = 1
 
 if znamienko == "+":
-    print(f"soucet cisel {prve_cislo} a {druhe_cislo} je {prve_cislo + druhe_cislo}")
+    print(f"Sucet cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo + druhe_cislo}")
 elif znamienko == "-":
-    print(f"rozdil cisel {prve_cislo} a {druhe_cislo} je {prve_cislo - druhe_cislo}")
+    print(f"Rozdiel cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo - druhe_cislo}")
 elif znamienko == "*":
-    print(f"soucin cisel {prve_cislo} a {druhe_cislo} je {prve_cislo * druhe_cislo}")
+    print(f"Krat cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo * druhe_cislo}")
 elif znamienko == "/":
-    print(f"podil cisel {prve_cislo} a {druhe_cislo} je {prve_cislo / druhe_cislo}")
+    try:
+        print(f"podiel cisiel {prve_cislo} a {druhe_cislo} je {prve_cislo / druhe_cislo}")
+    except (ZeroDivisionError):
+        print("nula sa neda delit")
 else:
-    print(f"zvolil jsi chybnou operaci, program bude ukoncen")
+    print(f"Zla operacia program bude ukonceny")
