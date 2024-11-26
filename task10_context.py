6a76f34163cdcd89ec8c1ca28635b7c18bce6db5
Milan <surkos.milan@gmail.com>
2024-11-26 19:44:16 +0100

    A: TimeMeasurement

--- task10_context.py
+++ task10_context.py
@@ -2,14 +2,14 @@ import time
 
 class TimeMeasurment:
     def __enter__(self):
-        print("Spúštam výpočet: ")
+        print(f"Spúštam výpočet")
         self.start_time = time.time()  # zaznam zaciatku casu
         return self
 
     def __exit__(self, a, b, c):
         self.end_time = time.time()  # Zaznam konca casu
         self.duration = self.end_time - self.start_time  # vypocet trvania
-        print(f"Blok trval {self.duration} sekund.")  # Print trvani v sekundach
+        print(f"Blok trval {self.duration:.3f} sekund.")  # Print trvani v sekundach
 
 
 with TimeMeasurment() as t:
